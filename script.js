3c67226b4b3076d1f1704981f9a7584257e95612
haJy <65567064+haJye@users.noreply.github.com>
2022-04-22 17:33:41 +0400

    haJy2

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let value1=document.querySelector("span").innerText
+let value2=document.querySelectorAll("span")[5].innerText
+
+let inputFirst=document.querySelector("input");
+let inputSecond =document.querySelectorAll("input")[1];
+
+console.log(inputFirst,inputSecond)
+
+let link = `https://api.exchangerate.host/latest?base=${value1}&symbols=${value2}`
+fetch(link)
+.then(res =>{
+    return res.json()
+})
+.then(data=>{
+    inputFirst.value=1;
+    inputSecond.value=Number(data.rates.USD)
+    console.log(data.rates.USD)
+})
 let btn1 = [...document.querySelectorAll("span")]
 
 btn1.slice(0,4).forEach(item=>{
@@ -7,6 +25,8 @@ btn1.slice(0,4).forEach(item=>{
                 item.classList.remove('active1')
        })
         e.target.classList.add('active1')
+        value1=e.target.innerText
+        console.log(value1);
     })
 })
 let btn2 = [...document.querySelectorAll("span")]
@@ -17,6 +37,29 @@ btn2.slice(4,8).forEach(item=>{
                 item.classList.remove('active2')
        })
         e.target.classList.add('active2')
+        value2=e.target.innerText
+        console.log(value2)
+    })
+})
+inputFirst.addEventListener('keyup',(e)=>{
+fetch(link)
+.then(res =>{
+    return res.json()
+})
+.then(data=>{
+    // inputSecond.value=Number(data.rates.USD)
+   inputSecond.value=e.target.value*data.rates[value2]
+})
+
+})
+inputSecond.addEventListener('keyup',(e)=>{
+    fetch(link)
+    .then(res =>{
+        return res.json()
+    })
+    .then(data=>{
+        // inputSecond.value=Number(data.rates.USD)
+       inputFirst.value=e.target.value*data.rates[value1]
     })
 })
 
