16293a9fc4920b19d22ab540600022f98b2d0067
haJy <65567064+haJye@users.noreply.github.com>
2022-04-22 16:52:34 +0400

    haJy1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let btn1 = [...document.querySelectorAll("span")]
+
+btn1.slice(0,4).forEach(item=>{
+    item.addEventListener("click",(e)=>{
+      
+       btn1.forEach(item=>{
+                item.classList.remove('active1')
+       })
+        e.target.classList.add('active1')
+    })
+})
+let btn2 = [...document.querySelectorAll("span")]
+btn2.slice(4,8).forEach(item=>{
+    item.addEventListener("click",(e)=>{
+      
+       btn2.forEach(item=>{
+                item.classList.remove('active2')
+       })
+        e.target.classList.add('active2')
+    })
+})
 
